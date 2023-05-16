cd657dde636141b446efeaf216b7e7bf1320de2c
Ennio <ennioh@icloud.com>
2023-05-15 20:55:34 -0400

    ScrollSuave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,30 +1,43 @@
 // A linha abaixo cria uma função para fazer um scroll suave nos links internos
-export default function initScrollSuave() {
+export default class ScrollSuave {
+	constructor(links, options) {
+		// Guardando todos os links internos em uma variável
+		this.linksInternos = document.querySelectorAll(links);
+		if (options === undefined) {
+			this.options = { behavior: "smooth", block: "start" };
+		} else {
+			this.options = options;
+		}
 
-    // Guardando todos os links internos em uma variável
-    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+		this.scrollToSection = this.scrollToSection.bind(this);
+	}
 
-    // A linha abaixo cria uma função que vai scrollar até a section do link interno
-    function scrollToSection(event) {
+	// A linha abaixo cria uma função que vai scrollar até a section do link interno
+	scrollToSection(event) {
+		// A linha abaixo preveni o padrão
+		event.preventDefault();
 
-      // A linha abaixo preveni o padrão
-      event.preventDefault();
+		// A linha abaixo guarda o href em uma variável
+		const href = event.currentTarget.getAttribute("href");
 
-      // A linha abaixo guarda o href em uma variável
-      const href = event.currentTarget.getAttribute("href");
+		const section = document.querySelector(href);
 
-      const section = document.querySelector(href);
+		// A linha abaixo cria uma função que vai fazer o scroll suave até a section clicada
+		section.scrollIntoView(this.options);
+	}
 
-      // A linha abaixo cria uma função que vai fazer o scroll suave até a section clicada
-      section.scrollIntoView({        
-        behavior: "smooth",
-        block: "start",
-      });
+	addLinkEvent() {
+		// foreach para cada link interno realizar o scroll
+		this.linksInternos.forEach((link) => {
+			link.addEventListener("click", this.scrollToSection);
+		});
+	}
+
+	init() {
+    if(this.linksInternos.length){
+
+      this.addLinkEvent();
     }
-    
-    // foreach para cada link interno realizar o scroll
-    linksInternos.forEach((link) => {
-      link.addEventListener("click", scrollToSection);
-    });
-  }
-  
+		return this;
+	}
+}
