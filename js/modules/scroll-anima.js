57262b7adc0b4943a262955165015752167f9d23
Ennio <ennioh@icloud.com>
2023-05-17 17:45:36 -0400

    Estilos de cursor e border adicionados em FAQ e no slide

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -3,9 +3,11 @@ import debounce from './debounce.js';
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.6;
+    // this.windowMetade = window.innerHeight * 0.60;
+    this.windowMetade = window.innerHeight * 0.60;
 
-    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
+    // this.checkDistance = debounce(this.checkDistance.bind(this), 50);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 15);
   }
 
   // Pega a distância de cada item em relação
